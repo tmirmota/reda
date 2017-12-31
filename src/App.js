2906a3919dcc78ee01d720721db09ddf307ca0a8
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-12-30 16:10:50 -0800

    Fixed styling of sidebar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import './App.css'
 import {withStyles} from 'material-ui/styles'
 import Switch from 'material-ui/Switch'
 import Button from 'material-ui/Button'
+import { FormControlLabel } from 'material-ui/Form'
+
+import { apiFetch } from './utils/apiUtils'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoidG1pcm1vdGEiLCJhIjoiY2phenpkeHl1MW5xcTJ2bWsxa2J2c3B1NCJ9.VzfA7MRGj7E8mdTSBdA4Rw'
@@ -41,7 +44,10 @@ const initialState = {
   folio: null,
   propertyDetails: null,
   propertyAddress: null,
-  selectedProperty: null
+  selectedProperty: null,
+  satellite: false,
+  neighborhood: null,
+  place: null
 }
 
 const styles = {
@@ -52,6 +58,23 @@ class App extends Component {
   state = initialState
   componentWillMount() {
     this.requestProperty(739184050000)
+  }
+  reverseGeocode = async lngLat => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngLat.lng},${lngLat.lat}.json?access_token=${mapboxgl.accessToken}`
+    const { json } = await apiFetch(url)
+    if (json) {
+      let neighborhood
+      let place
+      json.features.map(feature => {
+        const type = feature.place_type[0]
+        if (type === "neighborhood") {
+          neighborhood = feature.text
+        } else if (type === "place") {
+          place = feature.text
+        }
+      })
+      this.setState({ neighborhood, place })
+    }
   }
   requestProperty = async newFolio => {
     const { folio } = this.state
@@ -77,20 +100,18 @@ class App extends Component {
 
     if (checked) {
       map.addLayer({
-        id: 'zoning-fill',
+        id: 'zoning-line',
         source: 'zoning',
         'source-layer': 'zoning_districtsgeojson',
         minzoom: 11,
         maxzoom: 22,
-        type: 'fill',
+        type: 'line',
         paint: {
-          'fill-opacity': 0.2,
-          'fill-color': '#FFFFFF',
-          'fill-outline-color': '#f412da'
+          'line-color': '#f412da',
         }
       })
     } else {
-      map.removeLayer('zoning-fill')
+      map.removeLayer('zoning-line')
     }
 
     this.setState({ [name]: checked })
@@ -101,6 +122,219 @@ class App extends Component {
     map.setFilter(layer, ['==', filter, ''])
     this.setState({ selectedProperty: null})
   }
+  toggleSatellite = (event, checked) => {
+    const { map } = this.state
+
+    const layers = map.getStyle().layers
+
+    const sourceIds = ['census-tracts', 'properties', 'zoning']
+
+    const style = `mapbox://styles/mapbox/${checked ? 'satellite' : 'basic'}-v9`
+    map.setStyle(style)
+    map.on('style.load', () => {
+      const noSources = !map.getSource('census-tracts')
+      if (noSources) {
+        this.addSources(map)
+        layers.map(layer => {
+          if (sourceIds.includes(layer.source)) {
+            map.addLayer(layer)
+          }
+          return true
+        })
+      }
+    })
+    this.setState({ satellite: checked})
+  }
+  addSources = map => {
+    map
+      .addSource('census-tracts', {
+        type: 'vector',
+        url: 'mapbox://tmirmota.3u8wgv7d'
+      })
+      .addSource('properties', {
+        type: 'vector',
+        url: 'mapbox://tmirmota.69r2qz2u'
+      })
+      .addSource('zoning', {
+        type: 'vector',
+        url: 'mapbox://tmirmota.5h7gkfwq'
+      })
+  }
+  renderLayers = map => {
+    const fillColor = '#3f51b5'
+
+    this.addSources(map)
+
+    map.addLayer({
+      id: 'zoning-fill2',
+      source: 'zoning',
+      'source-layer': 'zoning_districtsgeojson',
+      minzoom: 11,
+      maxzoom: 22,
+      type: 'fill',
+      paint: {
+        'fill-opacity': 0.0,
+        'fill-color': '#FFFFFF'
+      }
+    })
+
+    sources.map(({ source, sourceLayer, maxzoom, minzoom, filter }) => {
+      map.addLayer(
+        {
+          id: `${source}-fill`,
+          source,
+          'source-layer': sourceLayer,
+          minzoom,
+          maxzoom,
+          type: 'fill',
+          paint: {
+            'fill-opacity': 0.2,
+            'fill-color': fillColor
+          }
+        }
+      )
+      map.addLayer(
+        {
+          id: `${source}-line`,
+          source,
+          'source-layer': sourceLayer,
+          minzoom,
+          maxzoom,
+          type: 'line',
+          paint: {
+            'line-color': '#7986cb'
+          }
+        }
+      )
+      map.addLayer(
+        {
+          id: `${source}-fill-hover`,
+          source,
+          'source-layer': sourceLayer,
+          type: 'fill',
+          paint: {
+            'fill-opacity': 0.5,
+            'fill-color': '#1de9b6'
+          },
+          filter: ['==', filter, '']
+        }
+      )
+      map.addLayer(
+        {
+          id: `${source}-line-hover`,
+          source,
+          'source-layer': sourceLayer,
+          type: 'line',
+          paint: {
+            'line-color': '#1de9b6'
+          },
+          filter: ['==', filter, '']
+        }
+      )
+      map.addLayer(
+        {
+          id: `${source}-fill-click`,
+          source,
+          'source-layer': sourceLayer,
+          type: 'fill',
+          paint: {
+            'fill-color': '#e9291d'
+          },
+          filter: ['==', filter, '']
+        }
+      )
+
+      map.on('click', `properties-fill`, e => {
+        console.log('Selected Property', e);
+        const feature = e.features[0]
+
+        const filterName = feature.properties[filter]
+        const layer = `${source}-fill-click`
+        if (filterName) {
+          map.setFilter(layer, ['==', filter, filterName])
+          const selectedProperty = {
+            filter,
+            layer
+          }
+          this.setState({ selectedProperty })
+        }
+
+      })
+      map.on('mousemove', `${source}-fill`, e => {
+        if (this.state.selectedProperty) {
+          console.log('Property is already selected');
+          return false
+        }
+
+        const feature = e.features[0]
+
+        const { source } = feature.layer
+
+        let zone
+        if (source === 'properties') {
+          const { Description } = feature.properties
+          const indexCivicNum = Description.indexOf('CIVIC_NUMBER:')
+          const indexStdStreet = Description.indexOf('STD_STREET:')
+          const indexPcoord = Description.indexOf('PCOORD:')
+          const indexSiteId = Description.indexOf('SITE_ID:')
+          const pcoord = Description.substring(
+            indexPcoord + 7,
+            indexSiteId - 1
+          )
+
+          const folio = pcoord * Math.pow(10, 12 - pcoord.length)
+          const streetNumber = Description.substring(
+            indexCivicNum + 13,
+            indexStdStreet - 1
+          )
+          const streetName = Description.substring(
+            indexStdStreet + 11,
+            indexPcoord - 1
+          )
+
+          const propertyAddress = {
+            number: streetNumber,
+            street: streetName
+          }
+
+          this.requestProperty(folio)
+
+          const zoneFeatures = map.queryRenderedFeatures(e.point, {
+            layers: ['zoning-fill2']
+          })
+          if (zoneFeatures[0]) {
+            const { Name, Description } = zoneFeatures[0].properties
+            const urlStart = Description.indexOf('zone_url') + 18
+            const urlEnd =
+              Description.substring(urlStart).indexOf('</td>') + urlStart
+            const url = Description.substring(urlStart, urlEnd)
+            zone = { name: Name, url }
+          }
+          this.setState({ propertyAddress })
+        } else if (source === 'census-tracts') {
+        }
+
+        const filterName = feature.properties[filter]
+        map.setFilter(`${source}-fill-hover`, ['==', filter, filterName])
+        map.setFilter(`${source}-line-hover`, ['==', filter, filterName])
+
+        this.reverseGeocode(e.lngLat)
+
+        this.setState({
+          properties: feature.properties,
+          lngLat: e.lngLat,
+          zone
+        })
+      })
+      map.on('mouseleave', `${source}-fill`, () => {
+        map.setFilter(`${source}-fill-hover`, ['==', filter, ''])
+        map.setFilter(`${source}-line-hover`, ['==', filter, ''])
+      })
+      return true
+    })
+
+    this.setState({ map })
+  }
   render() {
     const {
       lngLat,
@@ -108,15 +342,31 @@ class App extends Component {
       zone,
       propertyDetails,
       propertyAddress,
-      selectedProperty
+      selectedProperty,
+      satellite,
+      neighborhood,
+      place
     } = this.state
 
     const { classes } = this.props
 
+    console.log(window.innerWidth <= 768);
+
+    if (window.innerWidth <= 768) {
+      return (
+        <div className='container text-center mt-2'>
+          <h2>Head to your nearest desktop computer!</h2>
+          <p>
+            Sorry, Reda's interface isn't quite ready for mobile devices like yours.
+          </p>
+        </div>
+      )
+    }
+
     return (
       <div className="container-fluid h-100 no-bleed">
         <div className="row h-100">
-          <div className="col-2 pt-4 sidebar">
+          <div className="col pt-4 sidebar">
             {lngLat && (
               <div>
                 <div>
@@ -128,7 +378,7 @@ class App extends Component {
                     ) : (
                       ''
                     )}
-                    <div>Yaletown</div>
+                    <div className="text-muted"><span className="lead">{neighborhood}{neighborhood && place && ', '}</span>{place}</div>
                   </div>
                   <hr />
                   {propertyDetails && (
@@ -191,16 +441,21 @@ class App extends Component {
                       Remove Selection
                     </Button>
                   </div>}
-                  <p className="text-muted">
-                    Lat: {lngLat.lat}
-                    <br />Lng: {lngLat.lng}
-                  </p>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={satellite}
+                        onChange={this.toggleSatellite}
+                      />
+                    }
+                    label="Satellite"
+                  />
 
                 </div>
               </div>
             )}
           </div>
-          <div className="col col-10">
+          <div className="col">
             <div
               ref={el => (this.mapContainer = el)}
               className="absolute top right left bottom"
@@ -210,14 +465,22 @@ class App extends Component {
       </div>
     )
   }
+
+
   componentDidMount() {
+
+    if (window.innerWidth <= 768) {
+      return false
+    }
+
+
     const { lng, lat, zoom } = this.state
 
-    const fillColor = '#3f51b5'
 
     const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/tmirmota/cjb1fqagmg2r82srs6d5s9sew',
+      // style: 'mapbox://styles/tmirmota/cjb1fqagmg2r82srs6d5s9sew',
+      style: 'mapbox://styles/mapbox/basic-v9',
       center: [lng, lat],
       zoom
     })
@@ -228,198 +491,7 @@ class App extends Component {
     }));
 
     map.on('load', () => {
-      map
-        .addSource('census-tracts', {
-          type: 'vector',
-          url: 'mapbox://tmirmota.3u8wgv7d'
-        })
-        .addSource('properties', {
-          type: 'vector',
-          url: 'mapbox://tmirmota.69r2qz2u'
-        })
-        .addSource('zoning', {
-          type: 'vector',
-          url: 'mapbox://tmirmota.5h7gkfwq'
-        })
-
-      map.addLayer({
-        id: 'zoning-fill2',
-        source: 'zoning',
-        'source-layer': 'zoning_districtsgeojson',
-        minzoom: 11,
-        maxzoom: 22,
-        type: 'fill',
-        paint: {
-          'fill-opacity': 0.0,
-          'fill-color': '#FFFFFF'
-        }
-      })
-
-      sources.map(({ source, sourceLayer, maxzoom, minzoom, filter }) => {
-        map.addLayer(
-          {
-            id: `${source}-fill`,
-            source,
-            'source-layer': sourceLayer,
-            minzoom,
-            maxzoom,
-            type: 'fill',
-            paint: {
-              'fill-opacity': 0.2,
-              'fill-color': fillColor
-            }
-          },
-          'place_label_other'
-        )
-
-        map.addLayer(
-          {
-            id: `${source}-line`,
-            source,
-            'source-layer': sourceLayer,
-            minzoom,
-            maxzoom,
-            type: 'line',
-            paint: {
-              'line-color': '#7986cb'
-            }
-          },
-          'place_label_other'
-        )
-
-        map.addLayer(
-          {
-            id: `${source}-fill-hover`,
-            source,
-            'source-layer': sourceLayer,
-            type: 'fill',
-            paint: {
-              'fill-opacity': 0.5,
-              'fill-color': '#1de9b6'
-            },
-            filter: ['==', filter, '']
-          },
-          'place_label_other'
-        )
-
-        map.addLayer(
-          {
-            id: `${source}-line-hover`,
-            source,
-            'source-layer': sourceLayer,
-            type: 'line',
-            paint: {
-              'line-color': '#1de9b6'
-            },
-            filter: ['==', filter, '']
-          },
-          'place_label_other'
-        )
-
-        map.addLayer(
-          {
-            id: `${source}-fill-click`,
-            source,
-            'source-layer': sourceLayer,
-            type: 'fill',
-            paint: {
-              'fill-color': '#e9291d'
-            },
-            filter: ['==', filter, '']
-          },
-          'place_label_other'
-        )
-
-        map.on('click', `properties-fill`, e => {
-          console.log('Selected Property', e);
-          const feature = e.features[0]
-
-          const filterName = feature.properties[filter]
-          const layer = `${source}-fill-click`
-          if (filterName) {
-            map.setFilter(layer, ['==', filter, filterName])
-            const selectedProperty = {
-              filter,
-              layer
-            }
-            this.setState({ selectedProperty })
-          }
-
-        })
-
-        map.on('mousemove', `${source}-fill`, e => {
-          if (this.state.selectedProperty) {
-            console.log('Property is already selected');
-            return false
-          }
-
-          const feature = e.features[0]
-
-          const { source } = feature.layer
-
-          let zone
-          if (source === 'properties') {
-            const { Description } = feature.properties
-            const indexCivicNum = Description.indexOf('CIVIC_NUMBER:')
-            const indexStdStreet = Description.indexOf('STD_STREET:')
-            const indexPcoord = Description.indexOf('PCOORD:')
-            const indexSiteId = Description.indexOf('SITE_ID:')
-            const pcoord = Description.substring(
-              indexPcoord + 7,
-              indexSiteId - 1
-            )
-
-            const folio = pcoord * Math.pow(10, 12 - pcoord.length)
-            const streetNumber = Description.substring(
-              indexCivicNum + 13,
-              indexStdStreet - 1
-            )
-            const streetName = Description.substring(
-              indexStdStreet + 11,
-              indexPcoord - 1
-            )
-
-            const propertyAddress = {
-              number: streetNumber,
-              street: streetName
-            }
-
-            this.requestProperty(folio)
-
-            const zoneFeatures = map.queryRenderedFeatures(e.point, {
-              layers: ['zoning-fill2']
-            })
-            if (zoneFeatures[0]) {
-              const { Name, Description } = zoneFeatures[0].properties
-              const urlStart = Description.indexOf('zone_url') + 18
-              const urlEnd =
-                Description.substring(urlStart).indexOf('</td>') + urlStart
-              const url = Description.substring(urlStart, urlEnd)
-              zone = { name: Name, url }
-            }
-            this.setState({ propertyAddress })
-          } else if (source === 'census-tracts') {
-          }
-
-          const filterName = feature.properties[filter]
-          map.setFilter(`${source}-fill-hover`, ['==', filter, filterName])
-          map.setFilter(`${source}-line-hover`, ['==', filter, filterName])
-
-          this.setState({
-            properties: feature.properties,
-            lngLat: e.lngLat,
-            zone
-          })
-        })
-
-        map.on('mouseleave', `${source}-fill`, () => {
-          map.setFilter(`${source}-fill-hover`, ['==', filter, ''])
-          map.setFilter(`${source}-line-hover`, ['==', filter, ''])
-        })
-        return true
-      })
-
-      this.setState({ map })
+      this.renderLayers(map)
     })
 
     map.on('move', () => {
